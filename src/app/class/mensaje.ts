cba7727a70762e7cc5653e1fa72dd4899e072a75
daniel de brito <danieldebrito@outlook.com>
2023-06-09 01:02:20 -0300

    home

--- src/app/class/mensaje.ts
+++
@@ -1,9 +0,0 @@
-export class Mensaje {
-    constructor(
-      public id?: string,
-      public displayName?: string,
-      public mensaje?: string,
-      public fecha?: number,
-      public uid?: string, // llave del usr que mando el mensaje
-    ) { }
-  }
