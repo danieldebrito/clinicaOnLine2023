e181ddf6a373516284ac6fe7683e2b9feaa6684b
daniel de brito <danieldebrito@outlook.com>
2023-06-26 00:35:22 -0300

    especialidades

--- src/app/pages/especialidades/especialidades.module.ts
+++ src/app/pages/especialidades/especialidades.module.ts
@@ -6,6 +6,7 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { EspecialidadesListComponent } from './components/especialidades-list/especialidades-list.component';
 import { EspecialidadesGridComponent } from './components/especialidades-grid/especialidades-grid.component';
 import { EspecialidadesCardComponent } from './components/especialidades-card/especialidades-card.component';
+import { EspecialidadesItemComponent } from './components/especialidades-item/especialidades-item.component';
 
 
 @NgModule({
@@ -13,7 +14,8 @@ import { EspecialidadesCardComponent } from './components/especialidades-card/es
     EspecialidadesComponent,
     EspecialidadesListComponent,
     EspecialidadesGridComponent,
-    EspecialidadesCardComponent
+    EspecialidadesCardComponent,
+    EspecialidadesItemComponent
   ],
   imports: [
     CommonModule,
@@ -22,8 +24,11 @@ import { EspecialidadesCardComponent } from './components/especialidades-card/es
     ReactiveFormsModule
   ],
   exports: [
+    EspecialidadesComponent,
     EspecialidadesListComponent,
-    EspecialidadesGridComponent
+    EspecialidadesGridComponent,
+    EspecialidadesCardComponent,
+    EspecialidadesItemComponent
   ]
 })
 export class EspecialidadesModule { }
